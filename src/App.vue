c66cc21f04813f4fe7cdfaec51dd6ca63f4a89ef
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-02-17 01:16:21 +0000

    - inital commit - making request to back end to create bookings

---
+++ src/App.vue
@@ -0,0 +1,153 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useToast } from 'primevue/usetoast'
+import axios from 'axios'
+import router from '@/router'
+
+const name = ref('')
+const email = ref('')
+const phoneNumber = ref('')
+const vehicleMakeModel = ref('')
+const notes = ref('')
+
+const date = ref()
+const bookings = ref()
+const successMessage = ref('')
+const errorMessage = ref('')
+
+
+const toast = useToast()
+
+const createBooking = async () => {
+  try {
+    const userBooking = {
+      name: name.value,
+      email: email.value,
+      phone_number: phoneNumber.value,
+      vehicle_make_model: vehicleMakeModel.value,
+      booking_datetime: formatDate(date.value), // Format the date before sending
+      notes: notes.value // Format the date before sending
+    };
+
+    // Call the createBooking method from BookingService
+          const response = await axios.post('http://127.0.0.1:8000/admin/customerbookings', userBooking, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+            }
+          })
+          bookings.value = response.data
+    // Optionally, you can reset the form fields after successful submission
+    name.value = '';
+    email.value = '';
+    phoneNumber.value = '';
+    vehicleMakeModel.value = '';
+    date.value = null;
+    notes.value = '';
+    // Set success message
+    toast.add({
+      severity: 'success',
+      summary: 'Info',
+      detail: 'Booking created successfully',
+      life: 3000
+    });
+
+  } catch (error) {
+    console.error('Error creating booking:', error);
+    // Set error message
+    toast.add({
+      severity: 'error',
+      summary: 'Info',
+      detail: 'Booking couldn\'t be created. Please try again',
+      life: 3000
+    });
+  }
+};
+
+const formatDate = (date: any) => {
+  if (date) {
+    const formattedDate = new Date(date)
+    const year = formattedDate.getFullYear()
+    const month = String(formattedDate.getMonth() + 1).padStart(2, '0')
+    const day = String(formattedDate.getDate()).padStart(2, '0')
+    const hours = String(formattedDate.getHours()).padStart(2, '0')
+    const minutes = String(formattedDate.getMinutes()).padStart(2, '0')
+    const seconds = String(formattedDate.getSeconds()).padStart(2, '0')
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  }
+  return ''
+}
+
+onMounted(async () => {})
+</script>
+<template>
+  <div>
+    {{ bookings }}
+    <PrimeToast />
+    <div class="surface-section px-4 py-5 md:px-6 lg:px-8 mt-3">
+      <div
+        class="flex md:align-items-center md:justify-content-between flex-column md:flex-row pb-4 border-bottom-1 surface-border"
+      >
+        <div class="mb-3 lg:mb-0">
+          <div class="text-3xl font-medium text-900 mb-3">Create a booking</div>
+          <div class="text-500 mr-0 md:mr-3">
+            The garage is open between 9am and 5:30pm, Monday to Friday. Only 1 booking is allowed
+            per slot.
+          </div>
+        </div>
+        <span class="p-input-icon-left w-full md:w-auto">
+          <i class="pi pi-search"></i>
+          <InputText type="text" placeholder="Search" class="w-full md:w-auto" />
+        </span>
+      </div>
+      <div class="grid mt-5">
+        <div class="col-12 md:col-6">
+          <div class="p-fluid pr-0 md:pr-6">
+            <div class="field">
+              <label for="name" class="font-medium">Full name</label>
+              <InputText v-model="name" id="name" type="text" class="py-3 px-2 text-lg" />
+            </div>
+            <div class="field">
+              <label for="company" class="font-medium">Phone number</label>
+              <InputText v-model="phoneNumber" id="company" type="text" class="py-3 px-2 text-lg" />
+            </div>
+            <div class="field">
+              <label for="email" class="font-medium">Email</label>
+              <InputText v-model="email" id="email" type="text" class="py-3 px-2 text-lg" />
+            </div>
+            <div class="field">
+              <label for="message" class="font-medium">Vehicle make & model</label>
+              <InputText
+                v-model="vehicleMakeModel"
+                id="message"
+                :rows="6"
+                class="py-3 px-2 text-lg"
+              ></InputText>
+            </div>
+            <div class="field">
+              <label for="message" class="font-medium">Notes</label>
+              <PrimeTextarea v-model="notes" id="notes" class="pb-3"></PrimeTextarea>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 md:col-6 bg-no-repeat bg-right-bottom">
+          <div class="text-900 text-2xl font-medium mb-3">Please pick a date and Time</div>
+          <PrimeCalendar v-model="date" inline showTime hourFormat="12" />
+        </div>
+        <div class="col-12 md:col-6">
+          <PrimeButton
+            label="Create Booking"
+            icon="pi pi-send"
+            class="w-auto"
+            @click="createBooking"
+          ></PrimeButton>
+
+
+          {{ bookings }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
---
+++ src/main.ts
@@ -0,0 +1,82 @@
+import './assets/style.css'
+import 'primevue/resources/themes/aura-light-indigo/theme.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+const pinia = createPinia();
+import PrimeVue from 'primevue/config';
+
+import "primeflex/primeflex.css"
+import 'primeicons/primeicons.css'
+
+import StyleClass from 'primevue/styleclass';
+       
+import Ripple from 'primevue/ripple';
+
+import Button from "primevue/button"
+import InputText from 'primevue/inputtext';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Textarea from 'primevue/textarea';
+import Calendar from 'primevue/calendar';
+import Badge from 'primevue/badge';
+import Message from 'primevue/message';
+import Checkbox from 'primevue/checkbox';
+import Divider from 'primevue/divider';
+import Carousel from 'primevue/carousel';
+import VirtualScroller from 'primevue/virtualscroller';
+import Tag from 'primevue/tag';
+import Fieldset from 'primevue/fieldset';
+import Card from 'primevue/card';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import ProgressBar from 'primevue/progressbar';
+
+
+
+
+
+
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import BadgeDirective from 'primevue/badgedirective';
+import App from './App.vue'
+import router from './router'
+
+
+const app = createApp(App)
+
+
+app.use(ToastService);
+app.use(PrimeVue, { ripple: true, style: true });
+app.use(createPinia())
+app.use(router)
+
+app.directive('badge', BadgeDirective);
+app.directive('styleclass', StyleClass);
+app.directive('ripple', Ripple);
+
+
+app.component('InputText', InputText);
+app.component('PrimeButton', Button);
+app.component('DataTable', DataTable);
+app.component('PrimeColumn', Column);
+app.component('PrimeTextarea', Textarea);
+app.component('PrimeCalendar', Calendar);
+app.component('PrimeBadge', Badge);
+app.component('PrimeMessage', Message);
+app.component('PrimeToast', Toast);
+app.component('PrimeCheckbox', Checkbox);
+app.component('PrimeDivider', Divider);
+app.component('PrimeCarousel', Carousel);
+app.component('VirtualScroller', VirtualScroller);
+app.component('PrimeTag', Tag);
+app.component('PrimeFieldset', Fieldset);
+app.component('PrimeCard', Card);
+app.component('IconField', IconField);
+app.component('InputIcon', InputIcon);
+app.component('ProgressBar', ProgressBar);
+
+
+
+
+app.mount('#app')
