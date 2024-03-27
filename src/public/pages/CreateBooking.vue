3b0f1749d81c788dfba8fffb8123dfe17aaea5f1
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-27 20:11:32 +0000

    - update for netlify back end to railway

--- src/public/pages/CreateBooking.vue
+++ src/public/pages/CreateBooking.vue
@@ -29,7 +29,7 @@ const createBooking = async () => {
     };
 
     // Call the createBooking method from BookingService
-          const response = await axios.post('http://127.0.0.1:8000//admin/customerbooking', userBooking, {
+          const response = await axios.post(`${process.env.VUE_APP_BACKEND_URL}/admin/customerbooking`, userBooking, {
             headers: {
               'Content-Type': 'application/json',
               'Access-Control-Allow-Origin': '*'
