616b9c49fac7725c8f02dcd6fec867d3c2617bcf
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-06-05 10:41:15 +0100

    formatting for desktop and responsive

--- src/public/pages/VehicleDetails.vue
+++ src/public/pages/VehicleDetails.vue
@@ -42,64 +42,52 @@ onMounted(fetchCar)
 
 <template>
   <div class="surface-section px-5 py-5 md:px-6 lg:px-8">
-    <!-- <div class="text-3xl font-bold text-900 mb-4">{{ car?.make }} {{ car?.model }} {{ car?.variant }}</div> -->
 
-    <div class="grid">
-      <div class="col-12 md:col-12 lg:col-12 mx-auto">
-        <div v-if="car" class="max-w-7xl mx-auto">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <!-- Left: Main Image & Thumbnails -->
-            <div>
-              <div class="mb-4 border rounded overflow-hidden max-w-[400px] mx-auto">
-                <img :src="mainImage" alt="Main Image" class="w-full h-[250px] object-cover rounded" style="
-    border-radius: 12px;"/>
+    <div class="grid" v-if="car">
 
-              </div>
-              <div class="flex gap-3 overflow-x-auto max-w-[400px] mx-auto">
-                <img v-for="(img, idx) in images" :key="idx" :src="img" @click="mainImage = img"
-                  class="object-cover rounded cursor-pointer border-2" style="    width: 7rem;
-    height: 5rem;" :class="{ 'border-blue-500': mainImage === img, 'border-gray-300': mainImage !== img }" />
-              </div>
-            </div>
-
-            <!-- Right: Vehicle Info -->
-            <!-- Right: Vehicle Info -->
-            <div class="w-full">
-              <PrimeCard class="w-full">
-                <template #title>{{ car.make }} {{ car.model }} {{ car.variant }}</template>
-                <template #content>
-                  <div>
-                    <div>
-                      <PrimeTag :value="car.mileage?.toLocaleString() + ` Miles`" class="mb-2" />
-                    </div>
-                    <div>
-                      <PrimeTag :value="car.year" class="mb-2" />
-                    </div>
-                    <div>
-                      <PrimeTag :value="car.fuel_type" class="mb-2" />
-                    </div>
-                    <p class="text-gray-500 text-sm mb-3">Reg: {{ car.registration }}</p>
-                    <p class="text-2xl text-green-600 font-bold mb-4">£{{ car.price?.toLocaleString() }}</p>
-                    <p class="text-gray-700 text-sm leading-relaxed">
-                      {{ car.description }}
-                    </p>
-                    <PrimeButton label="Enquire / Book Test Drive"
-                      class="w-full bg-blue-600 border-blue-600 hover:bg-blue-700 text-white" />
-                  </div>
-                </template>
-              </PrimeCard>
-            </div>
-
-          </div>
-
-          <!-- Description -->
+      <!-- images and thumbail section -->
+      <div class="col-12 md:col-6 lg:col-8">
+        <div class="mb-4 border rounded overflow-hidden max-w-[200px] mx-auto">
+          <img :src="mainImage" alt="Main Image" class="w-full h-[250px] object-cover rounded" style="
+          border-radius: 12px;" />
 
         </div>
+        <div class="flex gap-3 overflow-x-auto max-w-[400px] mx-auto">
+          <img v-for="(img, idx) in images" :key="idx" :src="img" @click="mainImage = img"
+            class="object-cover rounded cursor-pointer border-2" style="    width: 7rem;
+          height: 5rem;" :class="{ 'border-blue-500': mainImage === img, 'border-gray-300': mainImage !== img }" />
+        </div>
+      </div>
 
-        <div v-else class="text-center py-12 text-gray-400">Loading vehicle details...</div>
+      <!-- vehicle details -->
+      <div class="col-12 md:col-6 lg:col-4">
+        <PrimeCard class="w-full">
+          <template #title>{{ car.make }} {{ car.model }} {{ car.variant }}</template>
+          <template #content>
+            <div>
+              <div>
+                <PrimeTag :value="car.mileage?.toLocaleString() + ` Miles`" class="mb-2" />
+              </div>
+              <div>
+                <PrimeTag :value="car.year" class="mb-2" />
+              </div>
+              <div>
+                <PrimeTag :value="car.fuel_type" class="mb-2" />
+              </div>
+              <p class="text-gray-500 text-sm mb-3">Reg: {{ car.registration }}</p>
+              <p class="text-2xl text-green-600 font-bold mb-4">£{{ car.price?.toLocaleString() }}</p>
+              <p class="text-gray-700 text-sm leading-relaxed">
+                {{ car.description }}
+              </p>
+              <PrimeButton label="Enquire / Book Test Drive"
+                class="w-full bg-blue-600 border-blue-600 hover:bg-blue-700 text-white" />
+            </div>
+          </template>
+        </PrimeCard>
+
       </div>
     </div>
-
+    <div v-else class="text-center py-12 text-gray-400">Loading vehicle details...</div>
   </div>
 
 </template>
